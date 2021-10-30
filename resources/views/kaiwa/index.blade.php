234d1e211a5c8a47f84a3d106da189b557e89bcc
229-9 <89262145+229-9@users.noreply.github.com>
2021-10-30 20:47:55 +0900

    [modify]style

--- resources/views/kaiwa/index.blade.php
+++ resources/views/kaiwa/index.blade.php
@@ -5,23 +5,24 @@
     </h2>
   </x-slot>
   <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="mystyle.css">
+    <link rel="stylesheet" href="{{asset('css/views.css')}}">
     </head>
 <body>
-    <h1>どのような話を希望ですか？</h1>
-    <hr>
-    <p1><a href="http://localhost/cat" class="btn">すべるお話</a><p1>
-    <p1><a href="http://localhost/nagomu" class="btn">場の空気が和むお話</a><p1>
-    <p1><a href="http://localhost/sekens" class="btn">世間話</a><p1>
-    <p1><a href="http://localhost/warai" class="btn">笑いをとるお話</a><p1>
-</form>
+  <h1>どのような話を希望ですか？</h1>
+
+  <div class="f-container">
+    <p1><a href="http://localhost/cat"class="btn btn--orange btn--radius">すべるお話</a><p1>
+    <p2><a href="http://localhost/nagomu"class="btn btn--orange btn--radius">場の空気が和むお話</a><p2>
+    <p3><a href="http://localhost/seken"class="btn btn--orange btn--radius">世間話</a><p3>
+    <p4><a href="http://localhost/warai"class="btn btn--orange btn--radius">笑いをとるお話</a><p4>
+  </div>
+  
 </body>
 </html>
 </x-app-layout>
-
--- resources/views/warai.blade.php
+++ resources/views/warai.blade.php
@@ -4,16 +4,17 @@
       {{ __('あなたにおすすめの面白いお話は') }}
     </h2>
   </x-slot>
+  
   <!DOCTYPE html>
-<html lang="en">
-<head>
+<html lang="jp">
+  <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="mystyle.css">
     </head>
-<body>
+  <body>
 <h1>{{ $contents }}です。</h1>
     <hr>
 </form>
